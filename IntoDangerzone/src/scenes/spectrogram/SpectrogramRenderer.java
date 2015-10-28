11588546fd693c93479c24aaba13211699734468
Jukka Eerikäinen <jukka.eerikainen@aalto.fi>
2015-10-28 21:58:26 +0200

    add spectrogram

---
+++ IntoDangerzone/src/scenes/spectrogram/SpectrogramRenderer.java
@@ -0,0 +1,19 @@
+package scenes.spectrogram;
+
+import processing.core.PApplet;
+import graphics.Renderer;
+
+public class SpectrogramRenderer extends Renderer {
+
+	public SpectrogramRenderer(PApplet parent) {
+		super(parent);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void render() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ IntoDangerzone/src/scenes/spectrogram/SpectrogramScene.java
@@ -0,0 +1,45 @@
+package scenes.spectrogram;
+
+import java.awt.KeyEventDispatcher;
+import java.awt.event.KeyEvent;
+
+import processing.core.PApplet;
+
+public class SpectrogramScene extends core.Scene implements KeyEventDispatcher {
+
+	public SpectrogramScene(PApplet parent) {
+		super(parent);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void update(float dtSeconds) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void render() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void activated() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deactivated() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean dispatchKeyEvent(KeyEvent e) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
